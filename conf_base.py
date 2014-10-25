43b1757eb47467e46fd22720b545a7ddf7c32677
Martin Landa <landa.martin@gmail.com>
2014-10-25 18:53:49 +0200

    logo cc

--- conf_base.py
+++ conf_base.py
@@ -36,7 +36,7 @@ extlinks = {'grasscmd': ('http://grass.osgeo.org/grass70/manuals/%s.html', ''),
             'epsg' : ('http://epsg.io/%s', 'EPSG:'), }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['../sphinx-template/_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
