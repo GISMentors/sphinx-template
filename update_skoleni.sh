5ec2dd7cdb09b9ea5d79957c613d00b23e0c955a
Martin Landa <landa.martin@gmail.com>
2015-11-14 16:56:36 +0100

    simplify update_skoleni

--- update_skoleni.sh
+++ update_skoleni.sh
@@ -13,20 +13,21 @@ function update_git {
         echo '-----------------------------------------------------------'
 	cd $1
 	git pull # --rebase
-	make clean
 };
 
 function update_html {
 	cd ~
 	echo updating $1
 	cd $1
-        make html$2
+	make clean
+        make html
 }
 
 function update_pdf {
     cd ~
     echo generating pdf for $1
     cd $1
+    make clean
     make latexpdf
     cp _build/latex/*.pdf html
     cd html
@@ -43,13 +44,11 @@ for dir in ${!workshops[*]}
 do
     workshop=${workshops[$dir]}
     update_git $workshop
+    update_html $workshop
     if [ $workshop == "grass-gis-zacatecnik" -o \
 	$workshop == "otevrena-geodata" -o \
 	$workshop == "postgis-zacatecnik" -o \
         $workshop == "qgis-zacatecnik" ] ; then
-        update_html $workshop full
 	update_pdf $workshop
-    else
-        update_html $workshop
     fi
 done
